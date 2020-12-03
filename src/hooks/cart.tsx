3bbed406a6ef915e23c2b53e393cfdd8bb111179
lucianolimafer <lucianolimaf3r@gmail.com>
2020-12-03 17:49:23 -0300

    :art: Get products and put on localStorage

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -30,7 +30,11 @@ const CartProvider: React.FC = ({ children }) => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const productsOnStoraged = await AsyncStorage.getItem('@GoMrkt:products');
+
+      if (productsOnStoraged) {
+        setProducts([...JSON.parse(productsOnStoraged)]);
+      }
     }
 
     loadProducts();
